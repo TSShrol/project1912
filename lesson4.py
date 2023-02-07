4870f77d79a6f50a84815bc27592a1484f7b9c18
TSShrol <shrolts@gmail.com>
2023-02-07 20:11:16 +0200

    add lesson5, working with string

--- lesson4.py
+++ lesson4.py
@@ -1,95 +1,113 @@
-# if value1:
-#     commands
-# elif value2:
-#     commands
+# # if value1:
+# #     commands
+# # elif value2:
+# #     commands
+# # else:
+# #     commands
+
+# t=int(input("Input t:"))
+# if t>18:
+#     print("shirt")
+# elif  t>=10 and t<=18:
+#     print("skirt")  
 # else:
-#     commands
-
-t=int(input("Input t:"))
-if t>18:
-    print("shirt")
-elif  t>=10 and t<=18:
-    print("skirt")  
-else:
-    print("warm jacket")
-
-# elif t<10:
 #     print("warm jacket")
 
-"""
-while умова:
-    команди
-else:       #доцільно, коли використовується break
-    команди
-"""
+# # elif t<10:
+# #     print("warm jacket")
 
-'''
-Дано два цілих числа A і B (при цьому A ≤ B). 
-Виведіть всі числа від A до B включно.
-'''
-a=int(input("input a:"))
-b=int(input("input b:"))
+# """
+# while умова:
+#     команди
+# else:       #доцільно, коли використовується break
+#     команди
+# """
 
-#1 to 4
-"""
-     a  b     a<=b    print(a)    a=a+1
- 1)  1  4     1<=4    print(1)    a=1+1 => 2
- 2)  2  4     2<=4    print(2)    a=2+1 => 3
- 3)  3  4     3<=4    print(3)    a=3+1 =>4
-4)   4  4     4<=4    print(4)    a=4+1 =>5
-5)  5   4     5<=4   
-"""
+# '''
+# Дано два цілих числа A і B (при цьому A ≤ B). 
+# Виведіть всі числа від A до B включно.
+# '''
+# a=int(input("input a:"))
+# b=int(input("input b:"))
 
-while a<=b:
-    print(a,end="\t\a")
-    a=a+1
+# #1 to 4
+# """
+#      a  b     a<=b    print(a)    a=a+1
+#  1)  1  4     1<=4    print(1)    a=1+1 => 2
+#  2)  2  4     2<=4    print(2)    a=2+1 => 3
+#  3)  3  4     3<=4    print(3)    a=3+1 =>4
+# 4)   4  4     4<=4    print(4)    a=4+1 =>5
+# 5)  5   4     5<=4   
+# """
+
+# while a<=b:
+#     print(a,end="\t\a")
+#     a=a+1
+# print()
+# print("the end cicle while")
+
+# """
+# for змінна in послідовність:
+#    команди
+
+# for змінна in послідовність:
+#    команди
+# else:
+#    команди
+# """
+# a=int(input("input a:"))
+# b=int(input("input b:"))
+# for number in range(a,b+1):
+#     print(number)
+
+# for letter in "Python":  # ['P','y','t','h','o','n']
+#     print(letter, end="\t")
+
+# """
+# Дано два цілих числа A і В. 
+# Виведіть всі числа від A до B включно, в порядку зростання, 
+# якщо A <B, або в порядку спадання в іншому випадку.
+# """
+# # 2   8       8   4
+# a=int(input("input a:"))
+# b=int(input("input b:"))
+
+# if a<b:
+#     while a<=b:
+#         print(a,end="\t\a")
+#         a=a+1
+# else:
+#     while a>=b:
+#         print(a,end="\t\a")
+#         a=a-1
+
+
+# if a<b:
+#     number=1
+# else:
+#     number=-1
+
+# for n in range(a,b,number):
+#     print(n, end="\t")
+
+# print()
+
+a=11
+b=6
+
+for i in range(a,b,-2):
+    print(i, end="\t")
+
 print()
-print("the end cicle while")
-
-"""
-for змінна in послідовність:
-   команди
-
-for змінна in послідовність:
-   команди
-else:
-   команди
-"""
-a=int(input("input a:"))
-b=int(input("input b:"))
-for number in range(a,b+1):
-    print(number)
-
-for letter in "Python":  # ['P','y','t','h','o','n']
-    print(letter, end="\t")
-
-"""
-Дано два цілих числа A і В. 
-Виведіть всі числа від A до B включно, в порядку зростання, 
-якщо A <B, або в порядку спадання в іншому випадку.
-"""
-# 2   8       8   4
-a=int(input("input a:"))
-b=int(input("input b:"))
-
-if a<b:
-    while a<=b:
-        print(a,end="\t\a")
-        a=a+1
-else:
-    while a>=b:
-        print(a,end="\t\a")
-        a=a-1
 
 
-if a<b:
-    number=1
-else:
-    number=-1
-
-for n in range(a,b,number):
-    print(n, end="\t")
+for i in range(a,b,-1):
+    if (i%2!=0):
+        print(i, end="\t")
 
 print()
+
+
+
  
     
---
+++ lesson5.py
@@ -0,0 +1,79 @@
+str1="python"
+str2="""JavaScript"""
+str3='C++'
+print(id(str1))
+strNew=str1.capitalize()
+# виведення значення змінної str1
+print(str1)
+# виведення адреси оперативної памяті,де зберігається str1
+print("id(str1) =",id(str1))
+
+print(strNew)
+print(id(strNew))
+
+# конкатенація або обєдання
+str1=str1+str2  # pythonJavaScript
+print(str1)
+print("id(str1) =",id(str1))
+
+print("Hello"*3)
+# Вирізати із str1= "pythonJavaScript"  підрядок "Java"
+print("str1= ",len(str1))
+strAboutJava=str1[6:10]
+print(strAboutJava)
+print(strAboutJava[::-1])
+print("first symbol =",str1[0])
+print("last symbol =",str1[-1])
+print("last symbol =",str1[len(str1)-1])
+
+#повернути код в ascii таблиці символів від 'a' (96) до 'z'   'A' (65)
+for symbol in 'abcdef'.upper():
+    print(ord(symbol),end="\t")
+
+print()
+#вивести символи кодів ascii таблиці від 30 до 100
+for kod in range(32,101):
+        print(chr(kod),end="\t")
+
+print()
+
+#Визначити чи  слово "Java" є у введеному рядку 
+inputStr=input("Введіть рядок:")
+if (inputStr.find("Java")>=0):
+    print("string 'Java' is finded")
+else:
+    print("string 'Java' isn't finded")
+
+"""
+   Знайдіть відому цитату, яка вам подобається, і збережіть у змінній citate. 
+   Збережіть ім’я автора вислову у змінній famous_person. 
+   Cкладіть повідомлення і збережіть його у новій змінній з ім’ям message.
+    Виведіть своє повідомлення. 
+    Результат повинен виглядати приблизно так (включаючи лапки): 
+    Albert Einstein once said, "A person who never made a mistake never tried anything new.".
+"""
+famous_person="Albert Einstein"
+citate="A person who never made a mistake never tried anything new"
+message=famous_person+" once said, \""+citate+"\""
+message1=f'{famous_person} once said, "{citate}"'
+message2=famous_person+' once said, "' +citate+'"'
+print(message)
+print(message1)
+print(message2)
+
+msg="""
+Збережіть ім’я користувача у змінній і додайте на початку і у кінці імені кілька пропусків.
+ Простежте за тим, щоб кожна керуюча послідовність (\t і \n) зустрічалася принаймні один раз. 
+ Виведіть ім’я, щоб було видно пропуски на початку і в кінці рядка. 
+Потім виведіть його знову з використанням кожної з функцій видалення пропусків: lstrip(), rstrip() і strip().
+"""
+userName="  Katrin   "
+print(userName)
+print(userName.strip())
+userName="****Katrin****"
+print(userName)
+print(userName.strip("*"))
+
+print(msg.split("."))
+
+
